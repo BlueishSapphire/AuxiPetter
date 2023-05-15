dff4ddb8b5920293ab519779fbcff9596191ffe9
NotRoy <sammycolonna@gmail.com>
2023-05-14 20:44:01 -0400

    Fixed timestamps

--- main.js
+++ main.js
@@ -2,9 +2,22 @@ const fs = require("fs");
 const { Client, Events, GatewayIntentBits, Partials } = require('discord.js');
 require("dotenv").config();
 
+function pad_zeroes(number, length) {
+	return number.toString().padStart(length, "0");
+}
+
 function timestamp(message) {
 	const date = new Date();
-	const datestring = `[${date.getFullYear()}-${date.getMonth()}-${date.getDate()} ${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}]`;
+
+	const year = pad_zeroes(date.getFullYear(), 2);
+	const month = pad_zeroes(date.getMonth(), 2);
+	const day = pad_zeroes(date.getDate(), 2);
+
+	const hour = pad_zeroes(date.getHours(), 2);
+	const minute = pad_zeroes(date.getMinutes(), 2);
+	const second = pad_zeroes(date.getSeconds(), 2);
+
+	const datestring = `[${year}-${month}-${day} ${hour}:${minute}:${second}]`;
 	console.info(datestring, message);
 }
 
